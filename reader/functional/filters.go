3670460f44a0f4caaec90275661fbab50b755eb0
Juan Carlos García <77403023+garcia-jc@users.noreply.github.com>
2023-04-11 22:01:25 -0600

    feat: tareas para ver inline fix: bench flags actualizadas

--- reader/functional/filters.go
+++ reader/functional/filters.go
@@ -31,55 +31,43 @@ func ReadItems(fn string) []Item {
 func isPublic(i Item) bool {
 	return i.Public
 }
+func publicRepos(items []Item) []Item {
+	return filter(items, isPublic)
+}
 
 func Work(items []Item, w io.Writer) int {
-	fmt.Fprintln(w, "total items", len(items))
+	count := 8
 	wg := new(sync.WaitGroup)
-	publicRepos := filter(items, isPublic)
-	fmt.Fprintln(w, "public repos", len(publicRepos))
-	func() {
-		wg.Add(1)
-		defer wg.Done()
-		mainBranch := filter(publicRepos, usesMasterBranch)
-		fmt.Fprintln(w, "public repos with main branch", len(mainBranch))
-	}()
-	func() {
-		wg.Add(1)
-		defer wg.Done()
-		initial := make(map[string]bool)
-		distinctAuthors := reduce(items, initial, countActors)
-		fmt.Fprintln(w, len(distinctAuthors), "distinct authors contribute to the public repos")
-	}()
-	func() {
-		wg.Add(1)
-		defer wg.Done()
-		private := func(in Item) bool {
-			return !in.Public
-		}
-		privateRepos := filter(items, private)
-		fmt.Fprintln(w, "private repos", len(privateRepos))
-	}()
-	func() {
-		wg.Add(1)
-		defer wg.Done()
-		gitRefs := forEach(items, func(in Item) string {
-			return in.Payload.RefType
-		})
-		refTypes := make(map[string]struct{})
-		refTypes = reduce(gitRefs, refTypes, groupByReferenceType)
-		fmt.Fprintf(w, "over %d distinct ref types across %d repos", len(refTypes), len(items))
-	}()
-	func() {
-		wg.Add(1)
-		defer wg.Done()
-		payloads := forEach(publicRepos, func(i Item) Payload {
-			return i.Payload
-		})
-		notMaster := filter(payloads, func(p Payload) bool {
-			return p.MasterBranch != "master"
-		})
-		fmt.Fprintln(w, "repos with a main branch not called ´master´", len(notMaster))
-	}()
+	wg.Add(count)
+	for i := 0; i < count; i++ {
+		func() {
+			defer wg.Done()
+			fmt.Fprintln(w, "total items", len(items))
+			mainBranch := filter(publicRepos(items), usesMasterBranch)
+			fmt.Fprintln(w, "public repos with main branch", len(mainBranch))
+			initial := make(map[string]bool)
+			distinctAuthors := reduce(items, initial, countActors)
+			fmt.Fprintln(w, len(distinctAuthors), "distinct authors contribute to the public repos")
+			private := func(in Item) bool {
+				return !in.Public
+			}
+			privateRepos := filter(items, private)
+			fmt.Fprintln(w, "private repos", len(privateRepos))
+			gitRefs := forEach(items, func(in Item) string {
+				return in.Payload.RefType
+			})
+			refTypes := make(map[string]struct{})
+			refTypes = reduce(gitRefs, refTypes, groupByReferenceType)
+			fmt.Fprintf(w, "over %d distinct ref types across %d repos", len(refTypes), len(items))
+			payloads := forEach(publicRepos(items), func(i Item) Payload {
+				return i.Payload
+			})
+			notMaster := filter(payloads, func(p Payload) bool {
+				return p.MasterBranch != "master"
+			})
+			fmt.Fprintln(w, "repos with a main branch not called ´master´", len(notMaster))
+		}()
+	}
 	wg.Wait()
 	return len(items)
 }
